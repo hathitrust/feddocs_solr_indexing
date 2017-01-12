5ace51d6949107d5731eddce3b63e5828b5755a9
Josh Steverman <jstever@umich.edu>
2017-01-12 10:48:00 -0500

    dumb bug

--- update_solr.rb
+++ update_solr.rb
@@ -6,4 +6,4 @@ si = SolrIndex.new( ENV['solr_host'], 9035 )
 num = si.update
 puts "regrecs updated #{num}"
 log = open('log/solr.update.log', 'a')
-log.puts "#{ENV['solr_host']}:9035\t#{num[:registry]}\t#{DateTime.now.to_s}"
+log.puts "#{ENV['solr_host']}:9035\t#{num}\t#{DateTime.now.to_s}"
